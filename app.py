aadb7bea02c9b945d40c86a4eb6197e06e4e1866
Charles Short <zulcss@gmail.com>
2018-03-08 10:38:28 -0500

    Add tickets
    Signed-off-by: Charles Short <zulcss@gmail.com>

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from rules.rule import Rule
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Rule, '/serviceAssurance/v1/ticketBySite/')
+api.add_resource(Rule, '/serviceAssurance/v1/ticketBySite/<site>',
+        '/serviceAssurance/v1/ticketBySite/')
 api.add_resource(Ticket, '/serviceAssurance/v1/tickets')
 
 if __name__ == '__main__':
--- rules/rule.py
+++ rules/rule.py
@@ -17,3 +17,22 @@ class Rule(Resource):
         return {
             'ticketRule': ticket
         }
+    
+    def get(self, site):
+        return {
+            "ticket": [
+                {
+                    "CLLI": site,
+                    "CaseType": "Infrasturcture NOC Trouble",
+                    "Escelation": "Level 1",
+                    "Owner": "Jones, Bob",
+                    "Queue": "StormTroopers",
+                    "Severity": "Medium",
+                    "SiteName": site,
+                    "SubType": "Hazcon",
+                    "TicketId": 2,
+                    "description": "Generator Running",
+                    "title": "AC-GEN RUN"
+                }
+        ]
+    }
